7f98b699734374342e3c4015f41536921d8a4c00
vladosadchuk <osadchukvlad19@gmail.com>
2025-05-30 23:02:48 +0300

    add websocket authorization support

--- backend/api/websocket.py
+++ backend/api/websocket.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from api.auth import get_current_user_from_token
 from database import get_db
 from sqlalchemy.orm import Session
 from models.models import Agent
@@ -6,24 +7,37 @@ import asyncio
 
 router = APIRouter()
 
-@router.websocket("/metrics/{hostname}")
-async def websocket_metrics(websocket: WebSocket, hostname: str, db: Session = Depends(get_db)):
+
+@router.websocket("/metrics/{agent_id}")
+async def websocket_metrics(websocket: WebSocket, agent_id: int, db: Session = Depends(get_db)):
     await websocket.accept()
+
+    token = websocket.cookies.get("auth_token")
+    if not token:
+        return await websocket.close(code=1008, reason="Auth token not found")
+
+    user = get_current_user_from_token(token, db)
+    if not user:
+        return await websocket.close(code=1008, reason="Invalid token")
+
+    if agent_id not in [agent.id for agent in user.agents]:
+        return await websocket.close(code=1008, reason="Forbidden access")
+
     try:
         while True:
-            agent = db.query(Agent).filter_by(hostname=hostname).first()
-            db.refresh(agent.metrics)
+            agent = db.query(Agent).filter_by(id=agent_id).first()
+            db.refresh(agent)
             if not agent or not agent.metrics:
                 await websocket.close()
                 break
             await websocket.send_json({
-                "hostname": hostname,
+                "hostname": agent.hostname,
                 "cpu_usage_percent": agent.metrics.cpu_usage_percent,
                 "memory_used_mb": agent.metrics.memory_used_mb,
                 "memory_used_percent": agent.metrics.memory_used_percent,
                 "memory_max": agent.metrics.memory_max,
                 "processes": agent.metrics.processes,
             })
-            await asyncio.sleep(3)
+            await asyncio.sleep(agent.update_time)
     except WebSocketDisconnect:
         print("WebSocket disconnected")
--- backend/main.py
+++ backend/main.py
@@ -6,14 +6,6 @@ from database import engine, Base
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
