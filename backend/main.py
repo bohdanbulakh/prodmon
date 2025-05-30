176f7d17bee91a3b5a9159ded3996a5fc000edc7
vladosadchuk <osadchukvlad19@gmail.com>
2025-05-31 00:29:11 +0300

    add cors

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-
+from fastapi.middleware.cors import CORSMiddleware
 from api import auth, agents, metrics, websocket
 from database import engine, Base
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
