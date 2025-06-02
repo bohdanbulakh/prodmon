3a094eecd87a8955ed9b845ac845139da27afbcc
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-02 21:27:44 +0300

    enhancement: reduced code duplication

--- web/src/app/(auth)/login/page.tsx
+++ web/src/app/(auth)/login/page.tsx
@@ -6,16 +6,8 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import AuthApi from '@/lib/api/AuthAPI';
 import Link from 'next/link';
-import AuthForm from '@/app/(auth)/components/AuthForm';
+import AuthForm, { formSchema } from '@/app/(auth)/components/AuthForm';
 import { useRouter } from 'next/navigation';
-
-const formSchema = z.object({
-  username: z.string().email({ message: 'Invalid email address' }),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters long' })
-    .regex(/[a-zA-Z0-9]/, { message: 'Password must be alphanumeric' }),
-});
 
 export default function LoginForm () {
   const queryClient = useQueryClient();
