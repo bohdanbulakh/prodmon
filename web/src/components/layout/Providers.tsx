7594eb708704e314ff089af527650e64ff915cac
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-02 23:51:14 +0300

    feature: created auth context

--- web/src/components/layout/Providers.tsx
+++ web/src/components/layout/Providers.tsx
@@ -3,11 +3,14 @@
 import { PropsWithChildren } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/api/queryClient';
+import AuthenticationProvider from '@/lib/providers/authentication/AuthenticationProvider';
 
 export function Providers ({ children }: PropsWithChildren) {
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <AuthenticationProvider>
+        {children}
+      </AuthenticationProvider>
     </QueryClientProvider>
   );
 }
---
+++ web/src/lib/types/authContext.ts
@@ -0,0 +1,4 @@
+export type AuthenticationContextType = {
+  loggedIn: boolean;
+  isLoading: boolean;
+};
