3b06cd2f1a85608c30d93223940ee4c2ead4336e
Bohdan Bulakh <bgdn.56t@gmail.com>
2025-06-04 00:51:54 +0300

    bugfix: fixed cors

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,14 @@ from database import engine, Base
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "https://prodmon.me",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
